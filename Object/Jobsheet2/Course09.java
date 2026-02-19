c258fc3b64e2ce4b677fc54702ba9a05ab2b74fc
lyniqna <fitriani.widodo18@gmail.com>
2026-02-19 22:15:42 +0700

    new

---
+++ Object/Jobsheet2/Course09.java
@@ -0,0 +1,33 @@
+package Object.Jobsheet02;
+
+public class Course09 {
+    String courseCode;
+    String name;
+    int sks;
+    int numberOfHours;
+
+    void showInformation() {
+        System.out.println("Course Name             : " + name);
+        System.out.println("Course Code             : " + courseCode);
+        System.out.println("Course Credit           : " + sks);
+        System.out.println("Total Meeting (h/week)  : " + numberOfHours);
+    }
+
+    void updateSKS(int sksNew) {
+        sksNew = sks;
+        System.out.println("The Course Credit have been changed.");
+    }
+
+    void addHours(int hours) {
+        hours += numberOfHours;
+    }
+
+    void reduceHours(int hours) {
+        if (hours > numberOfHours) {
+            System.out.println("The Number of hours left is sufficient to be reduced.");
+        } else {
+            numberOfHours -= hours;
+            System.out.println("Number of Hours: " + numberOfHours);
+        }
+    }
+}
