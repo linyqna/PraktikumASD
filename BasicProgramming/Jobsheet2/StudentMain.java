d9f8d7822bc0061beaa3f6815ddd49a88f3aadf1
lyniqna <fitriani.widodo18@gmail.com>
2026-02-19 11:03:48 +0700

    commit 1

---
+++ BasicProgramming/Jobsheet2/StudentMain.java
@@ -0,0 +1,16 @@
+package BasicProgramming.Jobsheet2;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student09 mhs1 = new Student09();
+        mhs1.name = "Muhammad Ali Farhan";
+        mhs1.nim = "22241720171";
+        mhs1.grade = "SI 2J";
+        mhs1.gpa = 3.55;
+
+        mhs1.informationDisplay();
+        mhs1.changeClass("SI 2K");
+        mhs1.updateGPA(3.60);
+        mhs1.informationDisplay();
+    }
+}
