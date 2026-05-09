3ffb938272851f6d2367207898d794f3cdb25108
lyniqna <fitriani.widodo18@gmail.com>
2026-05-09 16:04:31 +0700

    done

---
+++ LinkedList/Jobsheet12/Student.java
@@ -0,0 +1,19 @@
+package LinkedList.Jobsheet12;
+
+public class Student {
+    String nim, name, kelas;
+    double gpa;
+
+    public Student() {}
+
+    public Student(String nim, String name, String kelas, double gpa) {
+        this.name = name;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.gpa = gpa;
+    }
+
+    void print() {
+        System.out.println(nim + " - " + name + " - " + kelas + " - " + gpa);
+    }
+}
